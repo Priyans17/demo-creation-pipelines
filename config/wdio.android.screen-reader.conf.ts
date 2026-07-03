3f4bc31d1139694831e0797cceff9afd5884fd65
Priyans17 <priyans.mehta1712@gmail.com>
2026-07-04 00:12:12 +0530

    ScreenReader

---
+++ config/wdio.android.screen-reader.conf.ts
@@ -0,0 +1,26 @@
+import { config as sharedConfig } from "./wdio.shared.conf.ts";
+
+export const config: WebdriverIO.Config = {
+  ...sharedConfig,
+
+  specs: ["../test/specs/e2e-mobile/android/screen-reader.e2e.ts"], 
+
+  capabilities: [
+    {
+      platformName: "android",
+      "appium:deviceName": "Samsung Galaxy S23",
+      "appium:platformVersion": "13.0",
+      "appium:automationName": "UiAutomator2",
+      "appium:app": "bs://4c34f7e9cf20759ec358441dc5f9c7fb182c7e30",
+      "bstack:options": {
+        buildName: "Screen Reader - Android",
+        sessionName: "TalkBack Accessibility",
+        debug: true,
+        networkLogs: true,
+        deviceLogs: true,
+        appiumLogs: true,
+        video: true,
+      },
+    },
+  ],
+};
